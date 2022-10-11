7cae40864c4dc53abdef4b5be8fc155eaee3e91e
Yusef Feras Shakhtour <yfshakht@engr-ras-205.eos.ncsu.edu>
2022-10-11 08:17:28 -0400

    <Complete implementation of line.c & partial implementation of expand.c>

--- p3/expand.c
+++ p3/expand.c
@@ -1,21 +1,70 @@
 /**
   */
-
+#include <string.h>
 #include "expand.h"
-
+#include <stdio.h>
 
 bool wordChar(int ch) {
-return 1;
+  if ((ch >= 'A' && ch <= 'Z') ||
+      (ch >= '0' && ch <= '9') ||
+      (ch >= 'a' && ch <= 'z') || 
+      (ch == '_')) { 
+    return true;
+  }
+  else {
+    return false;
+  }
 
 }
 
 int expansionBound(char *sec, int maxRep) {
-return 1;
+  int totalCount; 
+  int charCount;
+  int pointer = 0;
 
+  while (sec[pointer] != EOF) {
+    if (wordChar(sec[pointer])) { 
+      totalCount++;
+      charCount++;
+      pointer++;
+    }
+    else {
+        totalCount++; 
+      if (charCount != 0 && charCount <= maxRep) { 
+        totalCount += maxRep - charCount;
+      }
+        charCount = 0;
+        pointer++;
+    }
+  }
+  return totalCount; 
 }
 
 void expand(char *src, char *dest, char *tList[], char *rList[], int pairs) {
 
+  int maxRep = 0;
+  for (int k = 0; k < sizeof(rList) / sizeof(rList[0]); k++) { 
+    if (strlen(rList[k]) > maxRep) {
+      maxRep = strlen(rList[k]);
+    }
+  }
+  char final[expansionBound(src, maxRep)]; 	 //Use this array to store the new, replaced line and then copy it to dest when done. 
+  char temp[100];    				 //"Holding" char array with 100 char buffer that is being used to compare the strings in tList.
+  for (int i = 0; i < sizeof(src) / sizeof(char); i++) {
+    
+    while (wordChar(src[i])) { 
+      temp[i] = src[i];
+    }
+    for (int j = 0; j < pairs; j++) {
+      if (strcmp(temp, rList[i]) == 0) { 
 
+        //Should do the replacement here.
+        strcpy(temp, rList[i]);
+      }
+    }
+
+    for (int i = strlen(final); i < strlen(temp); i++) {  //Now whatever is in temp needs to be copied over into destination, in this case to final
+      final[i] = temp[i];
+    }
+  }
 }
-
--- p3/line.c
+++ p3/line.c
@@ -5,12 +5,36 @@
 
 
 int measureLine(FILE *fp) {
-return 1;
-
+//  *fp = fopen(fp, "r");
+  char ch;
+  fscanf(fp, "%c", &ch);
+  int count = 0;
+  if (ch == '\n') {
+    count++;                //Newline is first char case.
+  }
+  while (ch != '\n') {
+    if (ch == EOF) {
+      break;                //No more input case, dont add to count
+    } 
+    count++;
+    fscanf(fp, "%c", &ch);
+    if (ch == '\n') {
+      count++;             //If at newLine char case
+    }
+  }
+  fseek(fp, -count, SEEK_CUR);
+  return count;
 }
 
 
 void readLine(FILE *fp, char str[]) {
-
-
+  int len = sizeof(str) / sizeof(char);
+  char ch;
+  for (int i = 0; i < len; i++) {
+    fscanf(fp, "%c", &ch);
+    str[i] = ch;
+  }
+  
+//  printf("%c\n", str[0]);
 }
+
